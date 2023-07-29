89111af3b49626b84719a34367a63d8a7ea56b34
Gerald Madlmayr <gerald.madlmayr@gmx.at>
2023-07-29 17:54:28 +0200

    Update Readme.MD; Update Dependencies.

--- README.md
+++ README.md
@@ -4,23 +4,37 @@ Addon for free@home to simulate a virtual weather station with data from a publi
 
 # References
 
-[Docu Busch und Jaeger](https://busch-jaeger.github.io/free-at-home-addon-development-kit-documentation-preview/)
+* [Docu Busch und Jaeger](https://busch-jaeger.github.io/free-at-home-addon-development-kit-documentation-preview/)
+* [Sample App](https://github.com/Busch-Jaeger/node-free-at-home-example)
+* [Free@Home Lib](https://github.com/Busch-Jaeger/node-free-at-home)
+* [ABB Portal](https://developer.eu.mybuildings.abb.com/tutorials)
 
-[Sample App](https://github.com/Busch-Jaeger/node-free-at-home-example)
+# Docs Infos
 
-[Free@Home Lib](https://github.com/Busch-Jaeger/node-free-at-home)
-
-[ABB Portal](https://developer.eu.mybuildings.abb.com/tutorials)
-
-setAutoKeepAlive: Every 2 min this [method](https://github.com/Busch-Jaeger/node-free-at-home/blob/e6a467ebb1fd31685c84b3fb27e777232ed5fd9f/src/freeAtHomeApi.ts#L275) is called automatically.
-
-isAutoConfirm automatically sets "OutputDataPoints". (?) using this [method](https://github.com/Busch-Jaeger/node-free-at-home/blob/e6a467ebb1fd31685c84b3fb27e777232ed5fd9f/src/freeAtHomeApi.ts#L215)
+* setAutoKeepAlive: Every 2 min this [method](https://github.com/Busch-Jaeger/node-free-at-home/blob/e6a467ebb1fd31685c84b3fb27e777232ed5fd9f/src/freeAtHomeApi.ts#L275) is called automatically.
+* isAutoConfirm automatically sets "OutputDataPoints". (?) using this [method](https://github.com/Busch-Jaeger/node-free-at-home/blob/e6a467ebb1fd31685c84b3fb27e777232ed5fd9f/src/freeAtHomeApi.ts#L215)
 
 # Meteomatric 
+
+Docu: https://www.meteomatics.com/en/api/request/api-requests-oauth-authentification/
+
+## Request
 
 * API Call: https://api.meteomatics.com/2023-07-26T12:19:00Z/t_2m:C,precip_1h:mm,wind_speed_10m:ms,wind_dir_10m:d,wind_gusts_10m_1h:ms,uv:idx/48.17496316252979,11.459006501063644/json
 * username = 'geraldmadlmayr_madlmayr'
 * password = 'maZM24tCv6'
 
+## Response
 
+```json
+{"version":"3.0","user":"geraldmadlmayr_madlmayr","dateGenerated":"2023-07-26T15:06:58Z","status":"OK","data":[
+    {"parameter":"t_2m:C","coordinates":[{"lat":48.174963,"lon":11.459007,"dates":[{"date":"2023-07-26T12:19:00Z","value":9.4}]}]},
+    {"parameter":"precip_1h:mm","coordinates":[{"lat":48.174963,"lon":11.459007,"dates":[{"date":"2023-07-26T12:19:00Z","value":0.23}]}]},
+    {"parameter":"wind_speed_10m:ms","coordinates":[{"lat":48.174963,"lon":11.459007,"dates":[{"date":"2023-07-26T12:19:00Z","value":3.2}]}]},
+    {"parameter":"wind_dir_10m:d","coordinates":[{"lat":48.174963,"lon":11.459007,"dates":[{"date":"2023-07-26T12:19:00Z","value":340.2}]}]},
+    {"parameter":"wind_gusts_10m_1h:ms","coordinates":[{"lat":48.174963,"lon":11.459007,"dates":[{"date":"2023-07-26T12:19:00Z","value":9.6}]}]},
+```
 
+# Package.json
+
+I ensure that the same versions from the NPM Pages in this lib are also used in the top-level [Free@Home Lib](https://github.com/Busch-Jaeger/node-free-at-home/blob/master/package.json)
--- src/main.ts
+++ src/main.ts
@@ -12,20 +12,12 @@ const password = 'maZM24tCv6'
 interface Token { access_token: string, token_type: string }
 
 // Defintion of the Weather Object we get from the API
-
-// {"version":"3.0","user":"geraldmadlmayr_madlmayr","dateGenerated":"2023-07-26T15:06:58Z","status":"OK","data":[
-//    {"parameter":"t_2m:C","coordinates":[{"lat":48.174963,"lon":11.459007,"dates":[{"date":"2023-07-26T12:19:00Z","value":9.4}]}]},
-//    {"parameter":"precip_1h:mm","coordinates":[{"lat":48.174963,"lon":11.459007,"dates":[{"date":"2023-07-26T12:19:00Z","value":0.23}]}]},
-//    {"parameter":"wind_speed_10m:ms","coordinates":[{"lat":48.174963,"lon":11.459007,"dates":[{"date":"2023-07-26T12:19:00Z","value":3.2}]}]},
-//    {"parameter":"wind_dir_10m:d","coordinates":[{"lat":48.174963,"lon":11.459007,"dates":[{"date":"2023-07-26T12:19:00Z","value":340.2}]}]},
-//    {"parameter":"wind_gusts_10m_1h:ms","coordinates":[{"lat":48.174963,"lon":11.459007,"dates":[{"date":"2023-07-26T12:19:00Z","value":9.6}]}]},
-
 interface WeatherResponse { version: string, user: string, dateGenerated: Date, status: string, data: WeatherData[] }
 interface WeatherData { parameter: string, coordinates: WeatherCoordindate[] }
 interface WeatherCoordindate { lat: string, long: string, dates: WeatherDateDataObj[] }
 interface WeatherDateDataObj { date: Date, value: number }
 
-// Guard Function
+// Basic Guard Function
 const isToken = (data: any): data is Token => {
   return typeof data.access_token == 'string' && typeof data.token_type == 'string'
 }
@@ -35,13 +27,6 @@ const isWeatherResponse = (data: any): data is WeatherResponse => {
 }
 
 async function main() {
-
-  // URL for Weather: https://api.meteomatics.com/2023-07-26T12:19:00Z/t_2m:C,precip_1h:mm,wind_speed_10m:ms,wind_dir_10m:d,wind_gusts_10m_1h:ms/48.17496316252979,11.459006501063644/json?access_token=<token>
-  // Benutzer: geraldmadlmayr_madlmayr
-  // Kennwort: maZM24tCv6
-
-  // https://www.meteomatics.com/en/api/request/api-requests-oauth-authentification/
-  
   const meteomatics = await freeAtHome.createWeatherStationDevice("VirtualWeatherStation", "meteomatics Wetter")
   meteomatics.wind.setAutoKeepAlive(true);
   meteomatics.wind.setAutoConfirm(true);
@@ -52,12 +37,12 @@ async function main() {
   meteomatics.rain.setAutoKeepAlive(true);
   meteomatics.rain.setAutoConfirm(true);
   meteomatics.rain.isAutoConfirm = true;
-
+  
   // we have 500 querys per day, so running ever 3 mintues
   var minutes = 3, the_interval = minutes  * 60  * 1000;
   setInterval(async function () {
-    console.log("3 Minutes.")
-    fetchAccessToken().then(token => fetchWeatherData(token).then(function (weatherResponse) {
+    console.log("Update Data.")
+    await fetchAccessToken().then(token => fetchWeatherData(token).then(function (weatherResponse) {
 
       let temperatureValue = weatherResponse.data.find(data => data.parameter == "t_2m:C")?.coordinates[0].dates[0].value;
       if(temperatureValue !== undefined){
@@ -93,48 +78,38 @@ async function main() {
  * The Token is value for 2 h, we we pull one for every call. 
  * @returns OAuth2 Token as a String
  */
-function fetchAccessToken() {
-  return fetch('https://login.meteomatics.com/api/v1/token', {
+async function fetchAccessToken() {
+  const resp = await fetch('https://login.meteomatics.com/api/v1/token', {
     method: 'GET', headers: { 'Authorization': 'Basic ' + Buffer.from(username + ":" + password).toString('base64') }
-  }).then((resp) =>
-    resp.json()
-  ).then((data) => {
-    // Use Type Guard.
-    if (isToken(data)) {
-      // With the Type Guard the assignement of the 
-      const phoebe: Token = data;
-      return phoebe.access_token;
-    } else {
-      throw Error('Parsed string is not an AccessToken 1.')
-    }
   });
+  const jsonResponse = await resp.json();
+  // Use Type Guard.
+  if (isToken(jsonResponse)) {
+    return jsonResponse.access_token;
+  } else {
+    throw Error('Parsed string is not an AccessToken.');
+  }
 }
-
-
 
 /** 
  * Method for Fetching the Weather Data from meteomatics
  * @returns WeatherResponseObjcect with the most current Data. 
  */
-function fetchWeatherData(oauth2Token: string) {
+async function fetchWeatherData(oauth2Token: string) {
 
-  // Fomat needs to be 2023-07-26T12:19:00Z
+  // Format needs to be 2023-07-26T12:19:00Z
   let dateString = new Date().toISOString(); 
-  var url = "https://api.meteomatics.com/" + dateString + "/t_2m:C,precip_1h:mm,wind_speed_10m:ms/48.17496316252979,11.459006501063644/json?access_token=" + oauth2Token;
-  return fetch(url, {
+  let url = "https://api.meteomatics.com/${dateString}/t_2m:C,precip_1h:mm,wind_speed_10m:ms/48.17496316252979,11.459006501063644/json?access_token=${oauth2Token}";
+  const resp = await fetch(url, {
     method: 'GET'
-  }).then(resp => {
-    return resp.json();
-  }).then(function (data) {
-    // Use Type Guard.
-    if (isWeatherResponse(data)) {
-      // With the Type Guard the assignement of the 
-      const phoebe: WeatherResponse = data;
-      return phoebe
-    } else {
-      throw Error('Parsed string is not an WeatherResponse.')
-    }
-  })
+  });
+  const jsonResponse = await resp.json();
+  // Use Type Guard.
+  if (isWeatherResponse(jsonResponse)) {
+    return jsonResponse;
+  } else {
+    throw Error('Parsed string is not an WeatherResponse.');
+  }
 }
 
 main();
